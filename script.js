21c996044eb826b81303c56a136193181f0b7acf
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-08-26 16:50:28 +0200

    corretto seguendo i consigli del video

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let tiri = prompt(`quanti tiri vuoi effettuare?`);
-let punteggioParziale = punteggioTiro;
-let punteggioTotale = punteggioParziale + punteggioTiro ;
 
-function punteggioTiro(giocatore) {
-    for (let i = 0; i < tiri; i++) {
-        let singleRoll = Math.floor(Math.random() * (6 - 1) + 1);
+// let punteggioParziale = punteggioTiro;
+
+function playDice(lanci) {
+    let player1= 0;
+    let player2= 0;
+
+    for (let i = 0; i < lanci; i++) {
+        let player1Roll = Math.floor(Math.random() * (6 - 1) + 1);
+        let player2Roll = Math.floor(Math.random() * (6 - 1) + 1);
+
+        player1 += player1Roll;
+        player2 += player2Roll;
     }
-    return punteggioTiro;   
+    if (player1 > player2) {       
+        return `player1 vince con un punteggio di ${player1}`;   
+    }else if (player2 > player1){
+        return `player2 vince con un punteggio di ${player2}`;
+    }else{
+        return `pareggio! Entrambi i giocatori hanno totalizzato ${player1}`
+    }
 }
 
-console.log(punteggioTiro);
-console.log(punteggioParziale);
+let tiri = Number(prompt(`quanti tiri vuoi effettuare?`));
+let punteggioTotale = playDice(tiri);
+
+
 console.log(punteggioTotale);
 
 
